e3c90b4276350d0ceac9192b625c11d88bbc69a1
loubaut <153442279+loubaut@users.noreply.github.com>
2025-03-26 11:41:54 +0100

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+connexion = sqlite3.connect()
+curseur = bdd.cursor()
+
+curseur.execute("""CREATE TABLE IF NOT EXIST scores(
+    
+    )""")
+
+donnees = []
+
+for donnee in donnees:
+    curseur.execute("")
+connexion.commit()
+
+
+
+
+
+
+connexion.commit()
+connexion.close()
+
+
